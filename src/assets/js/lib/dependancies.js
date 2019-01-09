bd244c53f0153e617a0b38a99e88ab0213294114
chris sammarco <csammarco@pavone.net>
2019-01-09 15:21:11 -0500

    remove extra code

--- src/assets/js/lib/dependancies.js
+++ src/assets/js/lib/dependancies.js
@@ -12,19 +12,3 @@ const deps = {
 };
 
 export default deps;
-
-
-// custom dependencies
-import AOS from 'aos';
-import svgInjector from 'svg-injector-2';
-import slick from 'slick-carousel';
-import TweenMax from "gsap/TweenMax";
-
-const deps = {
-  AOS: AOS,
-  svgInjector: svgInjector,
-  slickCarousel: slick,
-  TweenMax: TweenMax
-};
-
-export default deps;
